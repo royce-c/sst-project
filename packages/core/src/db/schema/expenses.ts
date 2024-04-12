082114931c36f82ec159c2f49062c4d78611596f
r <coviello.royce@gmail.com>
2024-04-11 20:51:30 -0700

    changed amount to description

--- packages/core/src/db/schema/expenses.ts
+++ packages/core/src/db/schema/expenses.ts
@@ -15,7 +15,7 @@ export const expenses = pgTable(
     id: serial("id").primaryKey(),
     userId: text("user_id").notNull(),
     title: varchar("title", { length: 1000 }).notNull(),
-    amount: numeric("amount", { precision: 10, scale: 2 }).notNull(),
+    description: numeric("description", { precision: 10, scale: 2 }).notNull(),
     date: date("date", { mode: "string" }).notNull(),
     imageUrl: text("image_url"),
     createdAt: timestamp("created_at", { withTimezone: true })
