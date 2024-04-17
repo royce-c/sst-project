d221d56ba541000436bb5a38842ffdaecda2f9d4
Royce <coviello@royce.gmail.com>
2024-04-17 15:04:24 -0700

    sort uploads by date, newest at the top

--- packages/web/src/routes/_authenticated/all-uploads.tsx
+++ packages/web/src/routes/_authenticated/all-uploads.tsx
@@ -3,12 +3,7 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
 export const Route = createFileRoute("/_authenticated/all-uploads")({
   component: Alluploads,
@@ -92,37 +87,46 @@ function Alluploads() {
                 </TableCell>
               </TableRow>
             ) : (
-              data.uploads.slice().reverse().map((upload, index) => (
-                <React.Fragment key={`upload-${index}`}>
-                  <div className="font-bold font-large text-center p-3">
-                    {upload.description}
-                  </div>
+              data.uploads
+                .sort(
+                  (a, b) =>
+                    new Date(b.date).getTime() - new Date(a.date).getTime()
+                )
+                .map((upload, index) => (
+                  <React.Fragment key={`upload-${index}`}>
+                    <div className="font-bold font-large text-center p-3">
+                      {upload.description}
+                    </div>
 
-                  <div className="p-4">
-                    {upload.imageUrl && (
-                      <img
-                        className="w-60 h-60 object-cover rounded-lg mx-auto"
-                        src={upload.imageUrl}
-                        alt={upload.title}
-                      />
-                    )}
-                  </div>
+                    <div className="p-4">
+                      {upload.imageUrl && (
+                        <img
+                          className="w-60 h-60 object-cover rounded-lg mx-auto"
+                          src={upload.imageUrl}
+                          alt={upload.title}
+                        />
+                      )}
+                    </div>
 
-                  <div className="flex items-center">
-                    <p className="mr-2 flex-grow"></p>
-                    <p className="text-right">{upload.date.split("T")[0]}</p>
-                  </div>
+                    <div className="flex items-center">
+                      <p className="mr-2 flex-grow"></p>
+                      <p className="text-right">{upload.date.split("T")[0]}</p>
+                    </div>
 
-                  <TableRow>
-                    <TableCell className="font-medium p-3" colSpan={3}>
-                      {upload.title}
-                      <button onClick={() => deleteUpload(upload.id)}
-                          className="h-4 w-4"> Delete
-                      </button>
-                    </TableCell>
-                  </TableRow>
-                </React.Fragment>
-              ))
+                    <TableRow>
+                      <TableCell className="font-medium p-3" colSpan={3}>
+                        {upload.title}
+                        <button
+                          onClick={() => deleteUpload(upload.id)}
+                          className="h-4 w-4"
+                        >
+                          {" "}
+                          Delete
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  </React.Fragment>
+                ))
             )}
           </TableBody>
         </Table>
